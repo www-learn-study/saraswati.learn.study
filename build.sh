f1da4d94e6ca48b8cc3f70730a23e32b7a5b976a
Michael Vorburger <mike@vorburger.ch>
2023-11-01 14:00:38 +0100

    content: First codelab!!

--- build.sh
+++ build.sh
@@ -7,3 +7,13 @@ mkdir -p BUILT/site/
 
 # TODO Rename site/ to static/
 find site/ -type f -exec ln -f {} BUILT/site \;
+
+# Go
+PATH="$PATH:$(go env GOPATH)/bin"
+
+# https://github.com/googlecodelabs/tools/tree/main/claat#install
+go install github.com/googlecodelabs/tools/claat@latest
+
+# TODO mv source, so that URL and directory match
+# (incl. changing "feedback link" in codelab.en.md)
+claat export -o BUILT/site/en/computers/intro learn/computers/setup/concepts/codelab.en.md
